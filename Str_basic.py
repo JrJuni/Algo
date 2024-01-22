e647bc7d9751c2b51827d51b1744e79fd5308daa
JrJuni <behomely0409@gmail.com>
2024-01-22 21:38:57 +0900

    앞으로 8시를 지키자, 21:30

--- Str_basic.py
+++ Str_basic.py
@@ -1,6 +1,21 @@
 '''
-'''
 # Q 27866
 S = str(input())
 n = int(input())
 print(S[n-1])
+
+# Q 2743
+S = str(input())
+print(len(S))
+'''
+
+
+# Q 9068
+def first_end(s):
+    print(s[0],end=" ")
+    print(s[-1])
+
+N = int(input())
+
+for i in range(N):
+    first_end(str(input()))
