b874609e30ca0050fa2d21d56eb8d2ecb2262b74
JrJuni <behomely0409@gmail.com>
2024-01-25 01:51:45 +0900

    저녁잠을 3시간을 처자네...

---
+++ Function.py
@@ -0,0 +1,4 @@
+def list_crack(cracktarget):
+    crack_ver = map(str, cracktarget)
+    crack_ver = ' '.join(crack_ver)
+    print(crack_ver)
--- Str_basic.py
+++ Str_basic.py
@@ -22,7 +22,7 @@ for i in range(N):
 c = str(input())
 ascii_value = ord(c)
 print(ascii_value)
-'''
+
 # Q 11720
 
 N = int(input())
@@ -33,4 +33,30 @@ for i in range(N):
     sum_str += int(S[i])
     
 print(sum_str)
+
+# Q 10809
+def list_crack(cracktarget):
+    crack_ver = map(str, cracktarget)
+    crack_ver = ' '.join(crack_ver)
+    print(crack_ver)
+
+letter_score = [-1]*26
+
+S = str(input())
+
+for i in range(len(S)):
+    temp_letter = ord(S[i]) - 97
+    if letter_score[temp_letter] == -1:
+        letter_score[temp_letter] = i
+    else:
+        continue
+    
+list_crack(letter_score)
+'''
+# Q 1152
+import sys
+S = list(map(str, sys.stdin.readline().split()))
+print(len(S))
+
+
 
