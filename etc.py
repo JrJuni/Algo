4341e854c4a8c01926597b51865350a0e062c1f7
JrJuni <behomely0409@gmail.com>
2024-02-04 23:30:30 +0900

    설에는 좀 하자

--- etc.py
+++ etc.py
@@ -27,7 +27,6 @@ for i in range(N-1):
     star = "*"*(2*(N-i)-3)
     print(blank, end="")
     print(star)
-''' 
 
 # Q 10250
 T = int(input())
@@ -49,6 +48,92 @@ for i in range(T):
     else:
         ho_str = str(ho)
     
-    print(str(fl))
     print(str(fl)+ho_str)
+
+# Q 2941
+S = str(input())
+position = 0
+cnt = 0
+
+while position < len(S):
+    if position == len(S)-1 :
+        cnt += 1
+        position += 1
+        break
     
+    elif S[position] == 'c':
+        if S[position+1] == '-':
+            cnt += 1
+            position += 2
+        elif S[position+1] == '=':
+            cnt += 1
+            position += 2
+        else:
+            cnt += 1
+            position += 1
+            
+    elif S[position] == 'd':
+        if S[position+1] == '-':
+            cnt += 1
+            position += 2
+        elif (S[position+1] == 'z'):
+            if position == len(S)-2 :
+                cnt += 2
+                position += 2
+            elif (S[position+2] == "="):
+                cnt += 1
+                position += 3
+            else:
+                cnt += 1
+                position += 1
+        else:
+            cnt += 1
+            position += 1
+            
+    elif S[position] == 'l':
+        if S[position+1] == 'j':
+            cnt += 1
+            position += 2
+        else:
+            cnt += 1
+            position += 1    
+
+    elif S[position] == 'n':
+        if S[position+1] == 'j':
+            cnt += 1
+            position += 2
+        else:
+            cnt += 1
+            position += 1  
+
+    elif S[position] == 's':
+        if S[position+1] == '=':
+            cnt += 1
+            position += 2
+        else:
+            cnt += 1
+            position += 1  
+
+    elif S[position] == 'z':
+        if S[position+1] == '=':
+            cnt += 1
+            position += 2
+        else:
+            cnt += 1
+            position += 1                        
+    else:
+        cnt += 1
+        position += 1
+
+print(cnt)
+
+# 파이썬의 re 라이브러리는 정규 표현식(Regular Expression)을 활용하여 문자열 패턴을 검색, 추출, 대체하는 기능을 제공하는 라이브러리입니다.
+
+re 라이브러리의 주요 함수와 메서드 몇 가지를 살펴보겠습니다:
+1. re.search(pattern, string): 주어진 문자열에서 정규 표현식 패턴에 매치되는 첫 번째 부분 문자열을 검색합니다.
+2. re.match(pattern, string): 주어진 문자열의 시작 부분에서 정규 표현식 패턴에 매치되는지 확인합니다.
+3. re.findall(pattern, string): 주어진 문자열에서 정규 표현식 패턴에 매치되는 모든 부분 문자열을 찾아 리스트로 반환합니다.
+4. re.sub(pattern, repl, string): 주어진 문자열에서 정규 표현식 패턴과 일치하는 부분을 다른 문자열로 대체합니다.
+
+''' 
+
---
+++ math.py
@@ -0,0 +1,31 @@
+'''
+# Q 1978
+N = int(input())
+
+prime = [1] * 1001
+prime[0] = 0
+prime[1] = 0
+
+def nonprime_killer (sample_list, denom):
+    for i in range(1, 1000//denom):
+        sample_list[denom*(i+1)] = 0
+        
+for j in range(2, 500):
+    nonprime_killer (prime, j)
+
+q_num = list(map(int, input().split()))
+cnt = 0
+
+for k in range(N):
+    if prime[q_num[k]] == 1:
+        cnt += 1
+    else:
+        continue
+
+print(cnt)
+
+# all 함수를 쓰면 더 간단히 정리할 수 있다.
+all은 하나라도 False가 있다면 False를 반환한다.
+
+'''
+
